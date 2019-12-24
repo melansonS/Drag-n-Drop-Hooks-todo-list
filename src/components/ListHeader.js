4d7b4afe25b5ae432792fa7f4de0e12e861a1667
melansonS <samuelmelanson@live.com>
2019-12-24 10:22:51 -0500

    cleaned up the imports, changed the font-familly

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { ListContext } from "../contexts/ListContext";
 
 const ListHeader = () => {
